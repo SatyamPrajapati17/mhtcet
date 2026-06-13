2eab1b24c79f1d6e4487ac5c163e9dfd5613335f
Shivam Prajapati <cricledge8292@gmail.com>
2026-06-13 18:13:33 +0530

    minor: match removeEventListener options for spec compliance

--- src/app/predict/page.tsx
+++ src/app/predict/page.tsx
@@ -89,7 +89,7 @@ export default function PredictPage() {
     document.addEventListener("touchstart", handleClick, { passive: true });
     return () => {
       document.removeEventListener("mousedown", handleClick);
-      document.removeEventListener("touchstart", handleClick);
+      document.removeEventListener("touchstart", handleClick, { passive: true });
     };
   }, [branchOpen]);
   const [tfws, setTfws] = useState(false);
